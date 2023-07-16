374e43bcda3caf6931fd7c06adaac47b3991664c
kc-clintone <kayseeclintone@gmail.com>
2023-07-16 15:18:41 -0700

    A program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,8 @@
 /**
  * main - The entry point of the program
  *
- * Description: A program that prints all possible combinations of single-digit numbers
+ * Description: A program that prints all possible
+ * combinations of single-digit numbers
  *
  * Return: Always 0 (Success)
 */
