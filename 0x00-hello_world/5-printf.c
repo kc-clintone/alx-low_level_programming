5ec8ba5c44e691c96c99a9011fe95b3b56477351
kc-clintone <kayseeclintone@gmail.com>
2023-07-16 13:02:08 +0000

    A C program that prints exactly with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program will print to the standard output using 'printf'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
