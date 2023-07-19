302ddf0fc85450e8a43d589fb14aee566044bae9
kc-clintone <kayseeclintone@gmail.com>
2023-07-19 23:40:03 +0000

    A program that prints the first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints out first 50 fibonacci numbers
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long n = 0, n1 = 1, n2;
+	
+	for (count = 0; count < 50; count++)
+	{
+		n2 = n + n1;
+		printf("%lu", n2);
+		n = n1;
+		n1 = n2;
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
