47576b75afd301322721d6730f70daf255d99f41
kc-clintone <kayseeclintone@gmail.com>
2023-07-19 21:40:35 +0000

    Modified the 4-isalphabet.c file - fixing bugs

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,10 +9,9 @@
  * @c: a aparameter that takes a function as input
 */
 
-int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
-		return (1);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	return (1);
+
 	return (0);
 }
