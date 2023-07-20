b0ef8aa02687cf3d68a177672277d2117b7bcedf
kc-clintone <kayseeclintone@gmail.com>
2023-07-20 22:42:41 +0000

    A function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - function that print all numbers
+* from 0 to 9
+*
+* Return: nothing
+*/
+
+void print_numbers(void)
+{
+	int n = 48;
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
