42c9df01b6a03da5a497ec8616e96fa483b14ee4
kc-clintone <kayseeclintone@gmail.com>
2023-07-21 00:35:46 +0000

    Fixed a bug on program >> 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,27 +9,22 @@
 
 int main(void)
 {
-	int num = 0;
+	int num;
 
 	while (num++ < 100)
-	{
 		if ((num % 3 == 0) && (num % 5 == 0))
-		{
 			printf("FizzBuzz ");
-		}
 		else if ((num % 3) == 0)
-		{
 			printf("Fizz ");
-		}
 		else if ((num % 5) == 0)
 		{
-			printf("Buzz ");
+			if (num != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
 		}
 		else
-		{
 			printf("%d ", num);
-		}
-	}
 	printf("\n");
 	return (0);
 }
