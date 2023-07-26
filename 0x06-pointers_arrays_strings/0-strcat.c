b205d15e86fa29cb431f2b13dbb67c141b47343d
kc-clintone <kayseeclintone@gmail.com>
2023-07-26 23:55:48 +0000

    Fixing a few bugs >> Forgot the brackets

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,8 +17,8 @@ char *_strcat(char *dest, char *src)
 int i = 0, dest_len = 0;
 
 while (dest[i++])
-        dest_len++;
+	dest_len++;
 for (i = 0; src[i]; i++)
-        dest[dest_len++] = src[i];
+	dest[dest_len++] = src[i];
 return (dest);
 }
