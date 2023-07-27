40600e7cc0788b94f26e90da5142e76e56ac0065
kc-clintone <kayseeclintone@gmail.com>
2023-07-27 20:56:18 +0000

    A function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,18 +2,24 @@
 #include <stdbool.h>
 
 /*
- * *cap_string - capitalizes te first letter of a
+ * cap_string - capitalizes te first letter of a
  * word in a given string
  *
+ * is_separator - stores the separaters
+ * Description: the cap_string converts a character
+ * to uppercase, while the is_separator returns all
+ * separators
  * @str: points to the char parameter
  *
  * Return: *str
 */
 
-bool is_separator(char c) {
-    return (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' || c == '.' ||
-            c == '!' || c == '?' || c == '"' || c == '(' || c == ')' || c == '{' ||
-            c == '}');
+bool is_separator(char c)
+{
+return (c == ' ' || c == '\t' || c == '\n' || c == ','
+|| c == ';' || c == '.' || c == '!' || c == '?' ||
+c == '"' || c == '(' || c == ')' || c == '{' ||
+c == '}');
 }
 
 char *cap_string(char *str)
