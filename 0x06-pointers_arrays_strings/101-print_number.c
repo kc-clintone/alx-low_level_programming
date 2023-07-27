9aed92f5c9b841a0c3ee2af1f568039a72633714
kc-clintone <kayseeclintone@gmail.com>
2023-07-27 21:01:18 +0000

    A function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,32 @@
 * @n: target integer
 *
 */
+
 void print_number(int n)
 {
-unsigned int x = n;
+	unsigned int m, d, count;
 
-if (n < 0)
-{
-_putchar('-');
-x = -x;
-}
-if ((x / 10) > 0)
-print_number(x / 10);
-_putchar((x % 10) + '0');
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
