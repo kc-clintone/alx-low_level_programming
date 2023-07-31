365040028e511fc0a96963ab0adb6041480b12c9
kc-clintone <kayseeclintone@gmail.com>
2023-07-31 22:54:26 +0000

    Create a file that contains the password for the crackme2 executable

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* set_string - a function that sets the value
+* of a pointer to a char
+* @s: the  pointer value
+* @to: the value pointed to
+*
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
