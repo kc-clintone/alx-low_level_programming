a7659b7c2141bb49676c9e26d922e000646505fe
kc-clintone <kayseeclintone@gmail.com>
2023-08-06 19:57:19 +0000

    Fixing bugs

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,31 +10,25 @@
 *
 * Return: 1 => error, 0 => success
 */
-int main(int argc, char *argv[])
-{
-int cents = atoi(argv[1]);
+int main(int argc, char *argv[]) {
+int cents;
 int coins[5] = {25, 10, 5, 2, 1};
 int num_coins = 0;
 int i;
 
-if (argc != 2)
-{
+if (argc != 2) {
 printf("Error\n");
 return (1);
 }
-
-if (cents < 0)
-{
+if (sscanf(argv[1], "%d", &cents) != 1 || cents < 0) {
 printf("0\n");
 return (0);
 }
-
-for (i = 0; i < 5; i++)
-{
+for (i = 0; i < 5; i++) {
 num_coins += cents / coins[i];
 cents %= coins[i];
 }
-
 printf("%d\n", num_coins);
 return (0);
 }
+
