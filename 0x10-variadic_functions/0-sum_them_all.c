0ed95347fad389e8a9003340e4872594c7ad5c12
kc-clintone <kayseeclintone@gmail.com>
2023-08-21 11:42:47 +0000

    made a few clicks to fix some issues

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
+	va_list list;
 	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(valist, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
+		sum += va_arg(list, int);
 
-	va_end(valist);
+	va_end(list);
 
 	return (sum);
 }
