4f4fcec02d71a87618009b62944df343eb398b20
kc-clintone <kayseeclintone@gmail.com>
2023-08-21 12:48:59 +0000

    modified 0-sum_them_all.c and variadic_functions.h files ==> trying to fix errors

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,19 @@
 #include "variadic_functions.h"
-
 /**
- * sum_them_all - returns the sum of all its parameters.
- * @n: amount of the arguments.
- *
- * Return: sum of its parameters.
+ * sum_them_all - returns the total args used
+ * @n: number of args
+ * Return: count of args
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
 
-	va_list args;
-	va_start(args, n);
+	va_list arg;
+	va_start(arg, n);
 	for (i = 0; i < n; ++i)
-	{
-		sum += va_arg(args, int);
-	}
-	va_end(args);
+		sum += va_arg(arg, int);
+	va_end(arg);
 	return (sum);
 }
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,9 +4,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/* Function prototypes */
 int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_floats(va_list arg);
+void print_string(va_list arg);
+void print_character(va_list arg);
+void print_integer(va_list arg);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+
+/* Structures 
+ *
+ * @ptc: print to console
+ * 
+ */
+typedef struct print_to_console{
+char *symbol;
+void (*ptc)(va_list arg);
+} print_to_console_t;
 
 #endif
