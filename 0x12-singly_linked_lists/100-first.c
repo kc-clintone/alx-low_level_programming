410a18e5351453f931dd6af77a0873c5c2afb367
kc-clintone <kayseeclintone@gmail.com>
2023-08-28 07:02:05 +0000

    modified 100-first.c fixing bugs

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
-#include "lists.h"
+#include <stdio.h>
 /**
- * _func - a function that  executes
+ * __attribute__ - constructor
+ * before - a function that  executes
  * before the main function does
  */
-void _func(void)
+void __attribute__((constructor)) before(void);
+void before(void)
 {
-printf("You're beat! and yet, you must allow,
-\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
