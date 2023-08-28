350e2f362f8906b83d7738e5d3ad0aa54bf8cfdc
kc-clintone <kayseeclintone@gmail.com>
2023-08-28 10:28:22 +0000

    modified lists.h and added a new file 5-free_listint2.c ==> a function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: Pointer to a pointer to the head of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return;
+
+	free_listint2(&((*head)->next));
+	free(*head);
+	*head = NULL;
+}
