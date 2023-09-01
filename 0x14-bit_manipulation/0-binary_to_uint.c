d2c35bfbfcc1b9110a88646402d640bcb5415d53
kc-clintone <kayseeclintone@gmail.com>
2023-09-01 22:36:48 +0000

    Added main.h and 0-binary_to_uint.c ==> a function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary_to_uint - Converts a binary string to an unsigned int.
+ * @b: Pointer to the binary string containing 0s and 1s.
+ *
+ * Return: The converted number, or 0 if there are
+ * invalid characters or if b is NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+	for (; *b != '\0'; b++)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		result = (result << 1) | (*b - '0');
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+/** function prototypes */
+unsigned int binary_to_uint(const char *b);
+#endif
