6252a84064c725a37d5bd65c5313d9463fb82d97
kc-clintone <kayseeclintone@gmail.com>
2023-09-04 22:27:33 +0000

    Added 3-cp.c ==>  a program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+#define BUFFER 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 #endif
