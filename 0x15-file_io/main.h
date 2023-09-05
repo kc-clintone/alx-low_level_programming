94fd044057b6324c16b72ec45d3f121a621a1bd1
kc-clintone <kayseeclintone@gmail.com>
2023-09-05 09:19:31 +0000

    100-elf_header.c ==> a program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-
+#include <elf.h>
 #define BUFFER 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
