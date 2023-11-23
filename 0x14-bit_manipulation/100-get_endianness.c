87acf9bd67e3966eb0945c0c45666a85134f168b
kc-clintone <kayseeclintone@gmail.com>
2023-11-23 23:58:58 +0000

    a function that checks the endianness of a system

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+unsigned int numbr = 1;
+unsigned char *byte_pointer = (unsigned char *)&numbr;
+unsigned int final_byte_value = *byte_pointer;
+
+/** Here, I Use bit manipulation to check the least
+significant bit (LSB) */
+return ((final_byte_value == 1) ? 1 : 0);
+}
+
