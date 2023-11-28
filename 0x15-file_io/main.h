7f459928633d2a205fe391c07c60ee54a69762c8
kc-clintone <kayseeclintone@gmail.com>
2023-11-28 08:47:45 +0000

    fixing some errors

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,5 +16,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(const char *message);
 
 #endif /* MAIN_H */
