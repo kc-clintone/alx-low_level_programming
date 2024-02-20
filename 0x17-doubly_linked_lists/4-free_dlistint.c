7049d43bf6263e8d9cf80db951a31f4696448583
kc-clintone <kayseeclintone@gmail.com>
2024-02-20 06:26:14 +0000

    0x17-doubly_linked_lists >>>> task [04]

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - A function that frees a dlistint_t list.
+ * @head: Head of the list.
+ * Return: Nada.
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *tmp;
+
+if (head != NULL)
+while (head->prev != NULL)
+head = head->prev;
+while ((tmp = head) != NULL)
+{
+head = head->next;
+free(tmp);
+}
+}
