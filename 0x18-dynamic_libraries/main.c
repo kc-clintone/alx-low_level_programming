5162b7418b7ede9bb35b76cdcfe2f7062b406d2a
kc-clintone <kayseeclintone@gmail.com>
2024-02-22 07:12:18 +0000

    initial setup and some crazy stuff

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "all_funcs.c"
+
+int main(void)
+{
+return (0);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
 /**----all the func prototypes*/
 
 int _putchar(char c);
