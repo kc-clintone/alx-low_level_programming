2b779b8c1833620826883a7f6ea6d1b71697dd2d
kc-clintone <kayseeclintone@gmail.com>
2024-02-25 07:54:44 +0000

    0x1A-hash_tables >>> rematch

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - A hash function fir implementing djb2 algorithm.
+ * @str: Target str.
+ * Return: Final hash.
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int constant;
+int index;
+
+constant = 5381;
+while ((index = *str++))
+constant = ((constant << 5) + constant) + index;
+return (constant);
+}
