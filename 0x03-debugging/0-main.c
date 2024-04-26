7112241a7555843ca594c10d8c7ddec18a7194ef
kc-clintone <kayseeclintone@gmail.com>
2024-04-26 22:33:51 +0000

    corrections

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,14 @@
 #include "main.h"
-
 /**
 * main - test function that prints if
 * integer is positive or negative
-*
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
 
 	i = 98 % 2;
 	positive_or_negative(i);
-
 	return (0);
 }
