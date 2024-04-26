c4bf289291a969c3c73c4c371633d2d6ad96f269
kc-clintone <kayseeclintone@gmail.com>
2024-04-26 22:29:52 +0000

    corrections

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
- * __attribute__ - constructor
- * before - a function that  executes
- * before the main function does
- */
-void __attribute__((constructor)) before(void);
+ * before - a function that  executes before the main function does
+*/
 void before(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
