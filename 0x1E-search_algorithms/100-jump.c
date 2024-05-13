23aeb461c9ce6aecf27dbcb0cc554d771d7c646c
kc-clintone <kayseeclintone@gmailcom>
2024-05-13 09:15:01 +0000

    0x1E-search_algorithms >> corrections

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -26,3 +26,14 @@ return (x);
 }
 return (-1);
 }
+
+/**
+ * min - Returns the minumum of two values
+ * @x: Target value
+ * @y: Target value
+ * Return: y: if y <= x, y otherwise
+*/
+size_t min(size_t x, size_t y)
+{
+return (x <= y ? x : y);
+}
