c2ef29e71936958c4d97fb0aa60b6982dbd03312
kc-clintone <kayseeclintone@gmailcom>
2024-05-13 06:59:10 +0000

    0x1E-search_algorithms >> corrections

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -42,6 +42,7 @@ typedef struct listint_s
 /**prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 size_t min(size_t x, size_t y);
 
 #endif /* SEARCH_ALGOS_H */
