52826a3a791c55731d3952718216b1fbe5d0752e
kc-clintone <kayseeclintone@gmail.com>
2024-08-02 14:57:37 +0000

    corrections

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-gcc *.c -c -fPIC && gcc *.o -shared -o liball.so
+gcc -Wall -Werror -Wextra -pedantic -fPIC -c all_funcs.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+rm *.o
